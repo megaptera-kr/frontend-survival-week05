cbb584b2d9c9d9b21acc1a20501416f9a3ce44ed
Minhye Kang <alsgp9898@naver.com>
2023-03-06 02:22:30 +0900

    fix: 핸들러에서 지연 시간 제거

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,6 @@ import App from './App';
 describe('App ', () => {
   it('renders without crash', async () => {
     render(<App />);
-
     await waitFor(() => {
       screen.getByText(/푸드코트 키오스크/);
       screen.getByText(/메리김밥/);
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -14,7 +14,7 @@ const handlers = [
   rest.get(`${BASE_URL}/restaurants`, (req, res, ctx) => {
     const { restaurants } = fixtures;
 
-    return res(ctx.status(200), ctx.delay(4000), ctx.json({ restaurants }));
+    return res(ctx.status(200), ctx.json({ restaurants }));
   }),
 
   rest.post<PostReqBody>(`${BASE_URL}/orders`, (req, res, ctx) => {
