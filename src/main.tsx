60b6a336a3e9e1749fde245aaf00feec5bb57683
Seung-wan <tmddhks0104@gmail.com>
2023-03-05 16:49:21 +0900

    fix: msw test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App';
 
 import worker from './mocks/server';
 
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
 function main() {
   const container = document.getElementById('root');
   if (!container) {
