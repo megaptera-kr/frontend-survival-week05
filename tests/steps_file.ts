c76a42c6c2967d644da13e128e7a5d6596209b50
hyejjun <hjlee9725@gmail.com>
2023-03-05 18:22:19 +0900

    delete playwright file, add sth on steps_file

--- tests/steps_file.ts
+++ tests/steps_file.ts
@@ -2,6 +2,12 @@
 // in this file you can append custom step methods to 'I' object
 
 module.exports = function () {
+  Feature('CodeceptJS demo');
+
+  Scenario('check Welcome page on site', ({ I }) => {
+    I.amOnPage('/');
+    I.see('푸드코트 키오스크');
+  });
   return actor({
 
     // Define custom steps here, use 'this' to access default methods of I.
