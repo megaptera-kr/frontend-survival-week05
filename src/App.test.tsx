e1adec99aa5e2f77a900293c585a068009cb64e5
Seung-wan <tmddhks0104@gmail.com>
2023-03-07 00:39:18 +0900

    fix: ci

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,6 @@ import { render, screen, waitFor } from '@testing-library/react';
 
 import App from './App';
 
-import fixtures from './fixtures';
-
-jest.mock('./hooks/useFetchRestaurants', () => () => fixtures.restaurants);
-
 describe('App ', () => {
   it('renders without crash', async () => {
     render(<App />);
