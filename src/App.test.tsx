ad21baa6caeb9d5985f1f6483f6b3a0936378d01
hyejjun <hjlee9725@gmail.com>
2023-03-06 15:35:07 +0900

    App.test.tsx 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-describe('App', async () => {
+describe('App', () => {
   it('render without crash', async () => {
     render(<App />);
     await waitFor(() => {
