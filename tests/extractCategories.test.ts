43f74d6a8cd75c2ea7fd1e2f6e5fa5da1d9bde32
OD <kg-te@naver.com>
2023-05-15 02:46:52 +0900

    ✅ 함수명 변경에 따라 테스트코드도 같이 수정

--- tests/extractCategories.test.ts
+++ tests/extractCategories.test.ts
@@ -1,12 +1,12 @@
 import restaurants from '../fixtures/restaurants';
-import makeCategory from '../src/utils/makeCategory';
+import extractCategories from '../src/utils/extractCategories';
 
 const context = describe;
 
-describe('makeCategory()', () => {
+describe('extractCategories()', () => {
   context('레스토랑 리스트를 받으면', () => {
     it('카테고리 목록을 중복 없이 추출하여 리턴한다', () => {
-      const categories = makeCategory(restaurants);
+      const categories = extractCategories(restaurants);
       expect(categories).toEqual(['중식', '한식', '일식']);
     });
   });
