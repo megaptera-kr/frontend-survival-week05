d814f2bf74a824a9ffa157a665477311b790359f
OD <kg-te@naver.com>
2023-05-15 02:43:16 +0900

    ♻️ makeCategory() 카테고리 추가할 때 중복 체크하도록 수정

--- src/utils/makeCategory.ts
+++ src/utils/makeCategory.ts
@@ -1,7 +1,8 @@
 import { Restaurant } from '../types/restaurant';
 
-export default function makeCategory(data : Restaurant []) {
-  return data.reduce((acc : string [], curr : Restaurant) => {
-    return [...acc, curr.category]
+export default function makeCategory(data: Restaurant[]) {
+  return data.reduce((acc: string[], curr: Restaurant) => {
+    const { category } = curr;
+    return acc.includes(category) ? acc : [...acc, curr.category];
   }, []);
 }
