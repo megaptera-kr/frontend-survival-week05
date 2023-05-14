3255b014a5664f960f2efc165df90facb9f4cc6f
OD <kg-te@naver.com>
2023-05-15 02:46:07 +0900

    ♻️ 함수명 변경 (makeCategory() 에서 extractCategories() 로)

--- src/utils/extractCategories.ts
+++ src/utils/extractCategories.ts
@@ -1,6 +1,6 @@
 import { Restaurant } from '../types/restaurant';
 
-export default function makeCategory(data: Restaurant[]) {
+export default function extractCategories(data: Restaurant[]) {
   return data.reduce((acc: string[], curr: Restaurant) => {
     const { category } = curr;
     return acc.includes(category) ? acc : [...acc, curr.category];
