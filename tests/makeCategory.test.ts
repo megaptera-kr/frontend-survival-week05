fda71af1f32362e05235ed1824db7e410252a1ea
OD <kg-te@naver.com>
2023-05-15 02:41:31 +0900

    💬 makeCategory() 테스트 텍스트 수정

--- tests/makeCategory.test.ts
+++ tests/makeCategory.test.ts
@@ -4,8 +4,8 @@ import makeCategory from '../src/utils/makeCategory';
 const context = describe;
 
 describe('makeCategory()', () => {
-  context('레스토랑 리스트를 ', () => {
-    it('카테고리 목록을 리턴한다', () => {
+  context('레스토랑 리스트를 받으면', () => {
+    it('카테고리 목록을 중복 없이 추출하여 리턴한다', () => {
       const categories = makeCategory(restaurants);
       expect(categories).toEqual(['중식', '한식', '일식']);
     });
