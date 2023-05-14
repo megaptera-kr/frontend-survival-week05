c0f9f6a60b7eb5e5c58ca9fe953e088d9ae47488
OD <kg-te@naver.com>
2023-05-14 23:32:20 +0900

    💡app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 export default function App() {
   return (
-    <h1>푸드코트 키오스크</h1>
+    <>
+      <section>점심 바구니</section>
+      <h1>푸드코트 키오스크</h1>
+      <section>영수증</section>
+    </>
   );
 }
