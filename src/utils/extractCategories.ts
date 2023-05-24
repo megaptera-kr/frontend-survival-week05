4e8cac0533a944a78c6c473dd2176303bd842262
(MJ) <(jomg6696@gmail.com)>
2023-05-24 23:23:02 +0900

    [조민구]5주차 과제-테스트 코드로 견고한 프로그램 만들기

--- src/utils/extractCategories.ts
+++ src/utils/extractCategories.ts
@@ -1,7 +1,7 @@
 import Restaurant from '../type/Restaurants';
 
 export default function extractCategories(restaurants: Restaurant[]):string[] {
-  return restaurants.reduce((acc:string, cur:Restaurant) => {
+  return restaurants.reduce((acc:string[], cur:Restaurant) => {
     const { category } = cur;
     return acc.includes(category) ? acc : [...acc, category];
   }, []);
