2b40986943acaf8d67b58442dc56362a629050e8
FEWoobIn <hwb0218@gmail.com>
2023-08-20 00:24:25 +0900

    MSW로 API 요청 mocking

--- src/__tests__/Restaurant.test.tsx
+++ src/__tests__/Restaurant.test.tsx
@@ -1,9 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import Restaurant from '../components/Restaurant';
+
+// jest.mock('../hooks/useFetchRestaurants.ts');
 
 describe('Restaurant', () => {
   it('renders restaurants', async () => {
     render(<Restaurant />);
+
+    await waitFor(() => {
+      screen.getByText(/메가반점/);
+      screen.getByText(/메리김밥/);
+    });
   });
 });
---
+++ src/mocks/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'import/no-extraneous-dependencies':
+    ['error',
+      { devDependencies: false, optionalDependencies: false, peerDependencies: false },
+    ],
+};
