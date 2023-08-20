e0b9128aa280a229032f583b5a9c5b4e34eded1f
함우성 <wooseongham@gmail.com>
2023-08-20 22:07:18 +0900

    서버 타입 추가

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { Menu, Receipt, Restaurant } from './types';
 
 const port = 3000;
 
@@ -8,14 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
 
-app.post('/orders', (req, res) => {
-  const { menu, totalPrice } = req.body;
+app.post('/orders', (req: Request, res: Response) => {
+  const { menu, totalPrice } = req.body as { menu: Menu[]; totalPrice: number };
 
-  const receipt = {
+  const receipt: Receipt = {
     id: Date.now().toString(),
     menu,
     totalPrice,
@@ -24,8 +25,8 @@ app.post('/orders', (req, res) => {
   res.status(201).send({ receipt });
 });
 
-app.get('/restaurants', (req, res) => {
-  const restaurants = [
+app.get('/restaurants', (req: Request, res: Response) => {
+  const restaurants: Restaurant[] = [
     {
       id: '1',
       category: '중식',
@@ -46,23 +47,55 @@ app.get('/restaurants', (req, res) => {
         { id: '6', name: '참치김밥', price: 4500 },
         { id: '7', name: '제육김밥', price: 5000 },
         { id: '8', name: '훈제오리김밥', price: 5500 },
+        { id: '9', name: '컵라면', price: 2000 },
       ],
     },
     {
       id: '3',
+      category: '한식',
+      name: '데브부엌',
+      menu: [
+        { id: '10', name: '제육덮밥', price: 10000 },
+        { id: '11', name: '닭떡국', price: 9000 },
+        { id: '12', name: '닭개장', price: 11000 },
+        { id: '13', name: '돈까스', price: 1000 },
+      ],
+    },
+    {
+      id: '4',
+      category: '일식',
+      name: '로드스시',
+      menu: [
+        { id: '14', name: '모듬초밥', price: 14000 },
+        { id: '15', name: '특선초밥', price: 17900 },
+        { id: '16', name: '스페셜초밥', price: 21000 },
+      ],
+    },
+    {
+      id: '5',
       category: '일식',
       name: '혹등고래카레',
       menu: [
-        { id: '9', name: '기본카레', price: 9000 },
-        { id: '10', name: '가라아게카레', price: 14000 },
-        { id: '11', name: '소시지카레', price: 13000 },
-        { id: '12', name: '돈까스카레', price: 14000 },
-        { id: '13', name: '닭가슴살카레', price: 13000 },
+        { id: '17', name: '기본카레', price: 9000 },
+        { id: '18', name: '가라아게카레', price: 14000 },
+        { id: '19', name: '소시지카레', price: 13000 },
+        { id: '20', name: '돈까스카레', price: 14000 },
+        { id: '21', name: '닭가슴살카레', price: 13000 },
+      ],
+    },
+    {
+      id: '6',
+      category: '한식',
+      name: '메가김치찌개',
+      menu: [
+        { id: '22', name: '김치찌개1인', price: 8000 },
+        { id: '23', name: '된장찌개', price: 8000 },
+        { id: '24', name: '계란말이', price: 6000 },
       ],
     },
   ];
 
-  res.send({ restaurants });
+  res.status(200).send({ restaurants });
 });
 
 app.listen(port, () => {
---
+++ express-app/types/index.ts
@@ -0,0 +1,17 @@
+export interface Menu {
+  id: string;
+  name: string;
+  price: number;
+}
+export interface Receipt {
+  id: string;
+  menu: Menu[];
+  totalPrice: number;
+}
+
+export interface Restaurant {
+  id: string;
+  category: string;
+  name: string;
+  menu: Menu[];
+}
