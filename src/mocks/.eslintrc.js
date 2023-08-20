8b8d2dc3c8bd6b91269f81091f490da12aeae122
FEWoobIn <hwb0218@gmail.com>
2023-08-20 19:52:18 +0900

    import/no-extraneous-dependencies

--- src/mocks/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'import/no-extraneous-dependencies':
-    ['error',
-      { devDependencies: false, optionalDependencies: false, peerDependencies: false },
-    ],
-};
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,5 @@
-import { rest } from 'msw'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw';
 import fixtures from '../../fixtures';
 
 const BASE_URL = 'http://localhost:3000';
@@ -8,10 +9,15 @@ const handlers = [
     const { restaurants } = fixtures;
     return res(ctx.status(200), ctx.json({ restaurants }));
   }),
-  rest.post(`${BASE_URL}/orders`, (req, res, ctx) => {
-    const { receipt } = fixtures
+  rest.post(`${BASE_URL}/orders`, async (req, res, ctx) => {
+    const { menu, totalPrice } = await req.json();
+    const receipt = {
+      id: Date.now().toString(),
+      menu,
+      totalPrice,
+    };
     return res(ctx.status(200), ctx.json({ receipt }));
-  })
+  }),
 ];
 
 export default handlers;
