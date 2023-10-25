950d6884b5db18f3238f5e5e310d995fe2476cf6
u-ryu-00 <lwh76104@gmail.com>
2023-10-25 16:00:29 +0900

    [류유정] 5주차 과제 - 테스트 코드로 견고한 프로그램 만들기

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, waitFor, screen } from '@testing-library/react';
 
 import App from './App';
 
-describe('App ', () => {
+describe('App', () => {
   it('renders restaurants', async () => {
     render(<App />);
 
