0156a2e80ec39ae80bd9dc966f047a23ff56bdd9
uhuikim <uhui94@naver.com>
2023-11-16 17:17:34 +0900

    feat : UI 마크업 기초 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
 export default function App() {
   return (
-    <h1>푸드코트 키오스크</h1>
+    <>
+      <h1>푸드코트 키오스크</h1>
+      <h2>점심 바구니</h2>
+      <button type="button">합계 : 0원 주문</button>
+
+      <div>
+        <label htmlFor="search">검색</label>
+        <input id="search" placeholder="식당 이름" />
+      </div>
+      <ul>
+        <li><button type="button">전체</button></li>
+        <li><button type="button">중식</button></li>
+        <li><button type="button">한식</button></li>
+        <li><button type="button">일식</button></li>
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              메가반점
+            </td>
+            <td>
+              중식
+            </td>
+            <td>
+              <ul>
+                <li>
+                  <span>짜장면(8000원)</span>
+                  <button type="button">선택</button>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        <p>[영수증 나오는 곳]</p>
+      </div>
+    </>
+
   );
 }
