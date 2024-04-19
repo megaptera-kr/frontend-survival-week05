ade28bffa180594f62cfd0a7032af512075c1aa6
WATA-Yiju <yiju.dev@watanow.com>
2024-04-19 16:51:07 +0900

    feat: useFetchRestaurants 훅 구현

---
+++ src/hooks/useFetchRestaurants.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import Restaurant from '../types/Restaurant';
+
+function useFetchRestaurants() {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  useEffect(() => {
+    async function fetchRestaurants() {
+      const url = 'http://localhost:3000/restaurants';
+      const response = await fetch(url);
+      const data = await response.json();
+      setRestaurants(data.restaurants);
+    }
+  }, []);
+
+  return restaurants;
+}
+
+export default useFetchRestaurants;
