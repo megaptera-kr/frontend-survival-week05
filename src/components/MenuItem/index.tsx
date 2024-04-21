68a8647c22eb0e12d49117473a731746f92510aa
loco9939 <kls9939@naver.com>
2024-04-21 21:30:32 +0900

    feat: useLocalStorage 사용하여 Orders에 추가 로직 구현

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,4 +1,7 @@
+import { useLocalStorage } from 'usehooks-ts';
+import { useRef } from 'react';
 import { convertKRW } from '../../utils';
+import Orders from '../../types/Orders';
 
 type MenuItemProps = {
   name:string;
@@ -6,12 +9,28 @@ type MenuItemProps = {
 }
 
 function MenuItem({ name, price }:MenuItemProps) {
+  const id = useRef<string>(`${name}-${new Date()}`);
+  const [orders, setOrders] = useLocalStorage<Orders>(
+    'orders',
+    { menu: [], totalPrice: 0 },
+  );
+
+  const addMenuItem = () => {
+    setOrders({
+      ...orders,
+      menu: [...orders.menu, {
+        id: id.current,
+        name,
+        price,
+      }],
+    });
+  };
   return (
-    <li>
-      <span>
+    <li style={{ marginBlock: '0.5rem' }}>
+      <span style={{ marginRight: '1rem' }}>
         {`${name}(${convertKRW(price)})`}
       </span>
-      <button type="button">선택</button>
+      <button type="button" onClick={addMenuItem}>선택</button>
     </li>
   );
 }
