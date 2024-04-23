a681d89dbfbd1d113da1d9f4aaf7d4c2df0f81ce
WATA-Yiju <yiju.dev@watanow.com>
2024-04-23 17:32:35 +0900

    feat(handlers): MSW POST /orders 테스트 코드 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,10 +4,18 @@ import fixtures from '../../fixtures';
 
 const BASE_URL = 'http://localhost:3000';
 
-const { restaurants } = fixtures;
+const { restaurants, receipt } = fixtures;
 
 const handlers = [
-  rest.get(`${BASE_URL}/restaurants`, (req, res, ctx) => res(ctx.status(200), ctx.json({ restaurants }))),
+  rest.get(`${BASE_URL}/restaurants`, (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({ restaurants }),
+  )),
+
+  rest.post(`${BASE_URL}/orders`, (req, res, ctx) => res(
+    ctx.status(201),
+    ctx.json({ receipt }),
+  )),
 ];
 
 export default handlers;
