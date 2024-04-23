a01eaa045033c355ea8d2021fceb1f2168a0558d
WATA-Yiju <yiju.dev@watanow.com>
2024-04-23 17:30:22 +0900

    feat(TextField): 테스트 코드 작성

--- src/components/TextField/TextField.test.tsx
+++ src/components/TextField/TextField.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import TextField from '.';
 
 const context = describe;
@@ -10,19 +10,33 @@ describe('TextField', () => {
   const text = '';
   const setText = jest.fn();
 
-  // when
-  context('label, placeholder props를 전달하면', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
     render(<TextField
       label={label}
       placeholder={placeholder}
       text={text}
       setText={setText}
     />);
+  });
 
+  // when
+  context('label, placeholder props를 전달하면', () => {
     // then
     it('label, placeholder props가 화면에 나타난다.', () => {
       screen.getByLabelText(label);
       screen.getByPlaceholderText(placeholder);
+    });
+  });
+
+  // when
+  context('사용자가 이름을 입력하면', () => {
+    // then
+    it('setText 함수를 호출한다.', () => {
+      fireEvent.change(screen.getByLabelText(label), {
+        target: { value: 'New Name' },
+      });
+      expect(setText).toHaveBeenCalledWith('New Name');
     });
   });
 });
